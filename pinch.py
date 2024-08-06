6f3d1ac30855e360a765bbb21cb460dc8df3c710
angymax <kvmanthansondigala@gmail.com>
2024-08-06 19:41:56 +0530

    recommit

--- pinch.py
+++ pinch.py
@@ -1,38 +1,40 @@
 import subprocess
+import os
 from pynput import keyboard
+import notify2
 
 # File to which the text will be appended
-MARKDOWN_FILE = ''
+MARKDOWN_FILE = os.path.expanduser('~/Documents/Max/pinch.md')
 
-# Function to read the current selection using xclip
+# Initialize the notify2 system
+notify2.init('Pinch')
+
 def get_selection():
     try:
+        # Use xclip to get the selected text from the primary clipboard
         result = subprocess.run(['xclip', '-selection', 'primary', '-o'], check=True, stdout=subprocess.PIPE)
         return result.stdout.decode('utf-8')
-    except subprocess.CalledProcessError as e:
+    except subprocess.CalledProcessError:
         notify_user("Error: Unable to access selection")
         return None
 
-# Function to notify user via GUI notification
 def notify_user(message):
-    subprocess.run(['notify-send', 'Pinch', message])
+    notification = notify2.Notification("Pinch", message)
+    notification.show()
 
-# Function to append text to the markdown file with two blank lines before the new text
 def append_to_file(text):
     try:
         with open(MARKDOWN_FILE, 'a') as file:
-            file.write('\n\n' + text + '\n')
+            file.write('-----'+'\n' + text + '\n')
         notify_user("Text successfully appended to markdown file")
     except Exception as e:
         notify_user(f"Error: {str(e)}")
 
-# Define the keyboard listener function
 def on_activate():
     selection_text = get_selection()
     if selection_text:
         append_to_file(selection_text)
 
-# Define the keyboard shortcut listener
 def for_canonical(f):
     return lambda k: f(listener.canonical(k))
 
@@ -46,7 +48,10 @@ listener = keyboard.Listener(
     on_release=for_canonical(hotkey.release)
 )
 
-# Start the keyboard listener in a background thread
-listener.start()
-notify_user("Pinch listener started. Press Ctrl+Shift+X to capture selected text.")
-listener.join()
+def main():
+    listener.start()
+    notify_user("Pinch listener started. Press Ctrl+Shift+X to capture selected text.")
+    listener.join()
+
+if __name__ == '__main__':
+    main()
